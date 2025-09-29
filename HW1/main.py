8ee39c3c5cbe858e08d1a9d3667d1f7a16795f54
Crista Falk <Crista2019@users.noreply.github.com>
2025-09-29 17:17:45 -0400

    exercise a done

--- HW1/main.py
+++ HW1/main.py
@@ -4,14 +4,28 @@ Homework 1: Modeling The Bystander Effect
 Crista Falk 10/3/2025
 """
 
+# imports
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
 # Exercise A – Modeling the Bystander Effect
 
-TEST
+def bystander_prediction(A, N):
+    """
+    Given N bystanders and a population "altruism" of A, return the probability of rescue.
+    This model uses an exponential distribution function over the number of bystanders, scaled by A.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    Inputs: N (int) <- number of bystanders,
+            A (float in range 0:1) <- Altruism level
+    Outputs: P (float in range 0:1) <- Predicted probability that someone is rescued
+    """
 
+    if N.any() == 0:
+        # if there are no bystanders, the person will definitely not be saved
+        return 0
+    # otherwise the model returns a predicted P(survival) which exponentially decreases when N increases and increases when A increases
+    return A * math.e ** (-A*N)
 
 # Exercise B – Supporting data
 
@@ -24,6 +38,28 @@ def print_hi(name):
 # Main function will walk through entire assignment
 if __name__ == '__main__':
     # Exercise A
+
+    # parameter test range
+    A_range = np.linspace(0, 1, 5)
+    N_range = np.linspace(0, 4, 5)
+
+    # show predictions in text
+    for A in A_range:
+        for N in N_range:
+            print(f"P(Survival| A={A}, N={N}) = ", bystander_prediction(A, N))
+        print("--")
+
+    # visualize how the probability of survival changes as N or A changes
+    A_values, N_values = np.meshgrid(A_range, N_range)
+    P_values = bystander_prediction(A_values, N_values)
+    # we want to vectorize our model to get many predictions at once given many possible input combinations
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(A_values, N_values, P_values, cmap='RdBu')
+    ax.set_xlabel('Altruism Level')
+    ax.set_ylabel('Number of Bystanders')
+    ax.set_zlabel('Probability of Survival')
+    plt.show()
 
     # Exercise B
 
