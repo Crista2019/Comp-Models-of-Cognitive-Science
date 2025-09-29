6e377e7007107e2bb933a69200390c131c1aca4f
Crista2019 <crista2019@gmail.com>
2025-09-29 16:12:05 -0400

    setting up

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+CS-134/PSY-141: Cognitive Modeling
+Homework 1: Modeling The Bystander Effect
+Crista Falk 10/3/2025
+"""
+
+# Exercise A – Modeling the Bystander Effect
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Exercise B – Supporting data
+
+# Exercise C – Falsifying data
+
+# Exercise D – the likelihood
+
+# Exercise E – Plotting MLL as function of A
+
+# Main function will walk through entire assignment
+if __name__ == '__main__':
+    # Exercise A
+
+    # Exercise B
+
+    # Exercise C
+
+    # Exercise D
+
+    # Exercise E
+    pass
+
