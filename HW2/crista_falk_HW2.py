c743d5c8857ae688022f0076c1fb120162d8543f
Crista2019 <crista2019@gmail.com>
2025-11-05 20:20:08 -0500

    adding mathematica script for parameter estimation

--- HW2/crista_falk_HW2.py
+++ HW2/crista_falk_HW2.py
@@ -1,5 +1,12 @@
-
-
+import pymc as pm
+import numpy as np
+import matplotlib.pyplot as plt
+import arviz as az
 
 if __name__ == "__main__":
-    pass
+    # Exercise A: Fitting the half-normal model
+    # Create a prior distribution for sigma such that:
+    # - only positive values have nonzero probability
+    # - the values of 1/sigma have a relatively wide range
+
+    # prior for the model parameter sigma found through Mathematica: sigma ~ Exp(1/.3)
